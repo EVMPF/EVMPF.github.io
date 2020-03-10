5710224b5931e3a3ebcdf4e1e3a24b624f6da174
TaWarX <a24833870@gmail.com>
2020-03-11 00:01:10 +0800

    EVMPF v1.5

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,6 +16,16 @@ $(document).ready(function () {
 
     if (screen.width <= 700) {
         document.getElementById('scheduleArrow').removeAttribute("class");
-        document.getElementById('scheduleArrow').setAttribute("class","big angle double down icon");
+        document.getElementById('scheduleArrow').setAttribute("class", "big angle double down icon");
+        document.getElementById('menuIcon1').removeAttribute("class");
+        document.getElementById('menuIcon1').setAttribute('class', "eight wide column");
+        document.getElementById('menuIcon2').removeAttribute("class");
+        document.getElementById('menuIcon2').setAttribute('class', "eight wide column");
+        document.getElementById('menuIcon3').removeAttribute("class");
+        document.getElementById('menuIcon3').setAttribute('class', "eight wide column");
+        document.getElementById('menuIcon4').removeAttribute("class");
+        document.getElementById('menuIcon4').setAttribute('class', "eight wide column");
+        // document.getElementById('footerLogoDiv').removeAttribute("class");
+        // document.getElementById('footerInfoDiv').removeAttribute('class');
     }
 });
