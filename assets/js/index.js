94b66570f53348257216014323415d0d1c6b71ef
E7 <a24833870@me.com>
2020-03-31 19:53:26 +0800

    EVMPF v2.0 backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
-    // fix menu when passed
-    $('.masthead').visibility({
-        once: false,
-        onBottomPassed: function () {
-            $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function () {
-            $('.fixed.menu').transition('fade out');
+    $('.ui .dropdown').dropdown();
+
+    document.oncontextmenu = new Function("return false");
+    oncontextmenu = "return false";
+
+    $(document).bind("contextmenu", function (e) {
+        if ("" != "write") {
+            return false;
         }
     });
 
-    // create sidebar and attach to menu open
-    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    document.body.oncopy = function () {
+        event.returnValue = false;
+    }
 
-
+    var scheduleRWDDiv = 
+                        '<div id="scheduleDiv" class="ui vertical center aligned">'+
+                            '<div id="schedule_1" role="img" class="banner_schedule_rwdimg_1 banner_rule" aria-label="banner schedule img"></div></div>'+
+                        '<div id="scheduleDiv" class="ui vertical center aligned">'+
+                        '<div id="schedule_1" role="img" class="banner_schedule_rwdimg_2 banner_rule" aria-label="banner schedule img"></div></div>';
     if (screen.width <= 700) {
-        document.getElementById('scheduleArrow').removeAttribute("class");
-        document.getElementById('scheduleArrow').setAttribute("class", "big angle double down icon");
+        // document.getElementById('scheduleArrow').removeAttribute("class");
+        // document.getElementById('scheduleArrow').setAttribute("class", "big angle double down icon");
         document.getElementById('menuIcon1').removeAttribute("class");
         document.getElementById('menuIcon1').setAttribute('class', "eight wide column");
         document.getElementById('menuIcon2').removeAttribute("class");
@@ -25,5 +30,7 @@ $(document).ready(function () {
         document.getElementById('menuIcon3').setAttribute('class', "eight wide column");
         document.getElementById('menuIcon4').removeAttribute("class");
         document.getElementById('menuIcon4').setAttribute('class', "eight wide column");
+        document.getElementById('schedule').removeAttribute("class");
+        document.getElementById('scheduleRWDDiv').innerHTML(scheduleRWDDiv);
     }
 });
